654b3f030341e658b14d01872181e6870bf8a0a3
laboratory10 <laboratory10@users.noreply.github.com>
2017-07-01 13:40:51 -0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var modal = document.getElementById("contact-modal")
-var btn = document.getElementById("contact-button")
-var span = document.getElementsByClassName("close")[0];
+window.onload = function(){
 
-btn.onClick = function() {
-	modal.style.display = "block";
-}
-span.onClick = function() {
-	modal.style.display = "none";
-}
-	
-window.onclick = function(event) {
-	if (event.target == modal) {
+	var modal = document.getElementById("contact-modal")
+	var btn = document.getElementById("contact-button")
+	var span = document.getElementsByClassName("close")[0];
+
+	btn.onClick = function() {
+		modal.style.display = "block";
+	}
+	span.onClick = function() {
 		modal.style.display = "none";
 	}
-}
+		
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 
-var testbtn = document.getElementById("test-button")
-testbtn.onClick = function() {
-	testbtn.value= "It worked!";
+	var testbtn = document.getElementById("test-button")
+	testbtn.onClick = function() {
+		testbtn.value= "It worked!";
+	}
 }
