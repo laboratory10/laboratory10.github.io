27794182a5d094d0cffc3c99ce09c48860d58a52
laboratory10 <laboratory10@users.noreply.github.com>
2017-07-01 19:42:58 -0700

    update

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ window.onload = function() {
 	var maintext = document.getElementById("main-content");
 	var container = document.getElementsByClassName("body");
 	
-	maintext.marginTop = ((container.style.height)-150)/2+"px"
+	maintext.style.marginTop = ((container.style.height)-150)/2+"px"
 }
