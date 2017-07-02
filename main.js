14aaab6bf1ef83ba14ff38a5957515956db36bb2
laboratory10 <laboratory10@users.noreply.github.com>
2017-07-01 21:47:12 -0700

    update

--- main.js
+++
@@ -1,6 +0,0 @@
-//window.onload = function() {
-	
-	//document.getElementById("main-content").marginTop = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
-	//document.getElementById("main-content").value = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
-	
-//}
