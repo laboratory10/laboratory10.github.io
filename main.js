1930f611e9a7391a5664f6577233dfc8f4c54320
laboratory10 <laboratory10@users.noreply.github.com>
2017-07-01 20:00:35 -0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	
-	maintext.marginTop = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
-	maintext.value = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
+	document.getElementById("main-content").marginTop = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
+	document.getElementById("main-content").value = ((document.getElementsByClassName("body").height-150)-document.getElementById("main-content").height)/2+"px";
 	
 }
